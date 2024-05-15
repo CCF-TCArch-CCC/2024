a9ab50bd5763da45221350ebaa314b1f56915ab7
lizz <lizz@shanghaitech.edu.cn>
2024-05-15 23:53:11 +0800

    2024

--- problems/fast/fast.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-#include <math.h>
-#include "fast.h"
-
-void fast_accel(DTYPE* img_in, int threshold, DTYPE* img_out, int rows, int cols)
-{
-    //Write your code here
-}
